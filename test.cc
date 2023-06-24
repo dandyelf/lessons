5e1091ad769c69cc193c4c92a1d2d869c0fc3813
leftrana <dandyelf32@gmail.com>
2023-06-24 19:04:05 +0300

    develop - fine

--- test.cc
+++ test.cc
@@ -9,6 +9,14 @@ void ThreadBar();
 
 template <typename T> T Sum(T a, T b) { return a + b; }
 
+static int b = 5;
+
+class StaticTest {
+public:
+  static int i;
+  int j = 5;
+}; // class StaticTest
+
 /**
  * The Singleton class defines the `GetInstance` method that serves as an
  * alternative to constructor and lets clients access the same instance of this
@@ -29,6 +37,7 @@ protected:
   std::string value_;
 
 public:
+  static int a;
   /**
    * Singletons should not be cloneable.
    */
@@ -54,10 +63,15 @@ public:
     std::cout << "Some important buisness function start." << this << std::endl;
   }
   std::string value() const { return value_; }
+  inline int GetCounter() const noexcept { return counter; };
+  inline void IncreaseCounter() { ++counter; };
+
+private:
+  int counter{};
 }; // Class Singleton
 
+int Singleton::a = 5;
 Singleton *Singleton::singleton_ = nullptr;
-
 /**
  * Static methods should be defined outside the class.
  */
@@ -72,11 +86,15 @@ Singleton *Singleton::GetInstance(const std::string &value) {
   return singleton_;
 }
 
+int StaticTest::i = 5;
+
 int main(int argc, char const *argv[]) {
   std::cout << "If you see the same value, then singleton was reused (yay!\n"
             << "If you see different values, then 2 singletons were created "
                "(booo!!)\n\n"
             << "RESULT:\n";
+  // Singleton *sing_first = Singleton::GetInstance("First");
+  // sing_first->SomeBusinessLogic();
   ThreadFoo();
   ThreadBar();
   if (argc > 0 || argv[0]) {
@@ -89,17 +107,24 @@ int main(int argc, char const *argv[]) {
   Singleton *sing2 = Singleton::GetInstance("BOOooom");
   std::cout << sing2->value() << std::endl;
   sing2->SomeBusinessLogic();
+
+  b = 6;
+  StaticTest::i = 6;
+  std::cout << StaticTest::i << std::endl;
   return 0;
 }
 
 void ThreadFoo() {
   // Following code emulates slow initialization.
   Singleton *singleton = Singleton::GetInstance("FOO");
-  std::cout << singleton->value() << "\n";
+  std::cout << singleton->value() << singleton->GetCounter() << std::endl;
+  singleton->SomeBusinessLogic();
 }
 
 void ThreadBar() {
   // Following code emulates slow initialization.
   Singleton *singleton = Singleton::GetInstance("BAR");
-  std::cout << singleton->value() << "\n";
+  singleton->IncreaseCounter();
+  std::cout << singleton->value() << singleton->GetCounter() << std::endl;
+  singleton->SomeBusinessLogic();
 }
